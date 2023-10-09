721aa31a4d3facdffd639ef4981b200ce8a3a4c8
Brian Wong <brianwong@crc-dot1x-nat-10-239-124-181.bu.edu>
2023-10-09 13:00:47 -0400

    edited main

--- hw3/main.py
+++ hw3/main.py
@@ -1,30 +1,27 @@
-from flask import escape
-import functions_framework
 from google.cloud import storage
+from flask import Flask, request, abort, send_file
 import logging
 
-storage_client = storage.Client()
+app = Flask(__name__)
 
 @functions_framework.http
+
 def file_server(request):
-    # Only allowing GET requests
-    if request.method != 'GET':
-        logging.error(f"Received unsupported method {request.method}")
-        return ("Not Implemented", 501)
-
-    filename = request.args.get('filename')
-
-    if not filename:
-        return ("File not specified", 400)
-
-    bucket = storage_client.bucket('bu-ds561-bwong778-hw2-bucket')
-    blob = bucket.blob(filename)
-
-    # Check if the file exists
-    if not blob.exists():
-        logging.warning(f"File not found: {filename}")
-        return ("Not Found", 404)
-
-    file_content = blob.download_as_text()
-
-    return file_content, 200
+    if request.method == 'GET':
+        try:
+            storage_client = storage.Client()
+            bucket = storage_client.bucket('bu-ds561-bwong778-hw2-bucket')
+            
+            blob = bucket.blob(request)
+            file_content = blob.download_as_text()
+            return send_file(file_content, attachment_filename=request, as_attachment=True), 200
+        
+        except FileNotFoundError:
+            logging.error(f"File not found: {request}")
+            abort(404)
+            
+    else:
+        logging.error(f"Unsupported method: {request.method} for file {request}")
+        abort(501)
+        
+            
