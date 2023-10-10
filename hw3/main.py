46784a9419f8d3b4972b106537f0255168e80157
Brian Wong <brianwong@crc-dot1x-nat-10-239-124-181.bu.edu>
2023-10-10 09:34:06 -0400

    app1 working

--- hw3/main.py
+++ hw3/main.py
@@ -1,4 +1,3 @@
-
 from google.cloud import storage
 from flask import Flask, request, abort, send_file
 import logging
@@ -8,28 +7,31 @@ import os
 app = Flask(__name__)
 
 @functions_framework.http
-
 def file_server(request):
+    
+    filename = request.path.lstrip('/')
+    filename = filename.replace('bu-ds561-bwong778-hw2-bucket/', '')
+    
     if request.method == 'GET':
         try:
             storage_client = storage.Client()
             bucket = storage_client.bucket('bu-ds561-bwong778-hw2-bucket')
             
-            blob = bucket.blob(request)
+            blob = bucket.blob(filename) 
             file_content = blob.download_as_text()
-            return send_file(file_content, attachment_filename=request, as_attachment=True), 200
+            
+            return send_file(file_content, attachment_filename=filename, as_attachment=True), 200
         
-        except FileNotFoundError:
-            logging.error(f"File not found: {request}")
-            abort(404)
+        except Exception as e:
+            logging.error(f"Error processing request: {e}")
+            abort(500)  
             
     else:
-        logging.error(f"Unsupported method: {request.method} for file {request}")
+        logging.error(f"Unsupported method: {request.method} for file {filename}")  
         abort(501)
         
             
 if __name__ =="__main__":
-    app.run(host="0.0.0.0", port = int(os.environ.get("PORT",8080)))
-
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
 
