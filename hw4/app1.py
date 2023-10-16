dfdce4f7c668eb33258daf0819f0ab879843576d
Brian Wong <brianwong@crc-dot1x-nat-10-239-124-181.bu.edu>
2023-10-16 18:58:59 -0400

    501 for all other requests

--- hw4/app1.py
+++ hw4/app1.py
@@ -13,19 +13,19 @@ BANNED_COUNTRIES = ["North Korea", "Iran", "Cuba", "Myanmar", "Iraq", "Libya", "
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(PROJECT_ID, TOPIC_NAME)
 
-@app.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
-@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+@app.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'CONNECT', 'OPTIONS', 'TRACE'])
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'CONNECT', 'OPTIONS', 'TRACE'])
 def file_server(path):
     filename = path.lstrip('/')
     filename = filename.replace('bu-ds561-bwong778-hw2-bucket/', '')
     
     country = request.headers.get('X-country')
     if country in BANNED_COUNTRIES:
-        logging.error('Banned Country:', 400 )
+        logging.error('Banned Country:', 400)
         error_message = f"Access attempt from banned country: {country}"
         publish_error(error_message)
 
-        return 'Banned country' , 400 
+        return 'Banned country', 400
     
     if request.method == 'GET':
         try:
@@ -33,10 +33,10 @@ def file_server(path):
             bucket = storage_client.bucket('bu-ds561-bwong778-hw2-bucket')
             
             blob = bucket.blob(filename) 
-            if (not blob.exists()):
-                logging.error('File not found:', 404 )
-                return 'File not found' , 404 
-                  
+            if not blob.exists():
+                logging.error('File not found:', 404)
+                return 'File not found', 404
+                
             file_content = blob.download_as_text()
             return file_content, 200
         
@@ -44,8 +44,8 @@ def file_server(path):
             logging.error('Error', 500)
             return 'Internal Server Error', 500
     else:
-        logging.error('Not Implemented:', 501 )
-        return 'Not Implemented' , 501 
+        logging.error('Not Implemented:', 501)
+        return 'Not Implemented', 501
 
 def publish_error(error_message):
     data = error_message.encode("utf-8")
@@ -53,6 +53,6 @@ def publish_error(error_message):
         publisher.publish(topic_path, data)
     except Exception as e:
         logging.error(f"Failed to publish error: {str(e)}")
-    
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
