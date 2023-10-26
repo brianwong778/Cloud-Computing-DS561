57f2bb4ea3263def2efebf557943e8de398e913b
Brian Wong <brianwong@crc-dot1x-nat-10-239-124-181.bu.edu>
2023-10-26 19:46:28 -0400

    testing now

--- hw5/app1.py
+++ hw5/app1.py
@@ -1,17 +1,65 @@
-from google.cloud import storage, pubsub_v1
-from flask import Flask, request, abort, send_file
+from flask import Flask, request, abort
+from google.cloud import storage
+from google.cloud.sql.connector import connector
+import pymysql
 import logging
 import os
-import requests
+from dotenv import load_dotenv  # Importing python-dotenv module
+
+load_dotenv()  # Loading environment variables from .env file
 
 app = Flask(__name__)
 
 PROJECT_ID = 'ds561-398719'
-TOPIC_NAME = 'error-topic'
 BANNED_COUNTRIES = ["North Korea", "Iran", "Cuba", "Myanmar", "Iraq", "Libya", "Sudan", "Zimbabwe", "Syria"]
 
-publisher = pubsub_v1.PublisherClient()
-topic_path = publisher.topic_path(PROJECT_ID, TOPIC_NAME)
+# Database connection function
+def get_db_connection():
+    conn = connector.connect(
+        os.environ.get("DB_CONNECTION_STRING"),  # fetching the connection string from the environment variable
+        "pymysql",
+        user=os.environ.get("DB_USER"),          # fetching the user from the environment variable
+        password=os.environ.get("DB_PASSWORD"),  # fetching the password from the environment variable
+        db=os.environ.get("DB_NAME"),            # fetching the database name from the environment variable
+    )
+    return conn
+
+# Function to insert request data into the database
+def insert_request_data(country, client_ip, gender, age, income, is_banned, time_of_day, requested_file):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    # Check if client_ip and country pairing exists
+    cursor.execute("SELECT 1 FROM CountryIPs WHERE client_ip = %s", (client_ip,))
+    if not cursor.fetchone():
+        cursor.execute("INSERT INTO CountryIPs (client_ip, country) VALUES (%s, %s)", (client_ip, country))
+
+    # Insert into Requests_Meta
+    cursor.execute("""
+        INSERT INTO hw5_Requests_Meta (client_ip, gender, age, income, is_banned, time_of_day) 
+        VALUES (%s, %s, %s, %s, %s, %s)
+    """, (client_ip, gender, age, income, is_banned, time_of_day))
+    
+    # Retrieve the request_id
+    request_id = cursor.lastrowid
+
+    # Insert into Requests_Files
+    cursor.execute("INSERT INTO hw5_Requests_Files (request_id, requested_file) VALUES (%s, %s)", (request_id, requested_file))
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+# Function to insert error data into the database
+def insert_error_data(time_of_request, requested_file, error_code):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    insert_query = """INSERT INTO hw5_Failed_Requests (time_of_request, requested_file, error_code)
+                      VALUES (%s, %s, %s)"""
+    cursor.execute(insert_query, (time_of_request, requested_file, error_code))
+    connection.commit()
+    cursor.close()
+    connection.close()
 
 @app.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'CONNECT', 'OPTIONS', 'TRACE'])
 @app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'CONNECT', 'OPTIONS', 'TRACE'])
@@ -19,14 +67,23 @@ def file_server(path):
     filename = path.lstrip('/')
     filename = filename.replace('bu-ds561-bwong778-hw2-bucket/', '')
     
+    # Extract information from request headers or request data
     country = request.headers.get('X-country')
-    if country in BANNED_COUNTRIES:
-        logging.error('Banned Country:', 400)
-        error_message = f"Access attempt from banned country: {country}"
-        publish_error(error_message)
+    client_ip = request.headers.get('X-client-IP')  
+    gender = request.headers.get('X-gender') 
+    age = request.headers.get('X-age')
+    income = request.headers.get('X-income')
+    is_banned = country in BANNED_COUNTRIES
+    time_of_day = request.headers.get('X-time')  
+    requested_file = filename
 
+
+    # Handle request and send response
+    if is_banned:
+        logging.error('Banned Country:', 400)
+        insert_error_data(time_of_day, requested_file, 400)
         return 'Banned country', 400
-    
+
     if request.method == 'GET':
         try:
             storage_client = storage.Client()
@@ -35,26 +92,22 @@ def file_server(path):
             blob = bucket.blob(filename) 
             if not blob.exists():
                 logging.error('File not found:', 404)
+                insert_error_data(time_of_day, requested_file, 404)
                 return 'File not found', 404
                 
             file_content = blob.download_as_text()
+            # Save request data to database
+            insert_request_data(country, client_ip, gender, age, income, is_banned, time_of_day, requested_file)
             return file_content, 200
         
         except Exception as e:
             logging.error('Error', 500)
+            insert_error_data(time_of_day, requested_file, 500)
             return 'Internal Server Error', 500
     else:
         logging.error('Not Implemented:', 501)
+        insert_error_data(time_of_day, requested_file, 501)
         return 'Not Implemented', 501
-
-
-
-def publish_error(error_message):
-    data = error_message.encode("utf-8")
-    try:
-        publisher.publish(topic_path, data)
-    except Exception as e:
-        logging.error(f"Failed to publish error: {str(e)}")
-
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
