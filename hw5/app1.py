48ffab3c58929d3ff2fb9620df53f657785b648d
Brian Wong <brianwong@crc-dot1x-nat-10-239-124-181.bu.edu>
2023-10-27 11:00:41 -0400

    added back pubsub

--- hw5/app1.py
+++ hw5/app1.py
@@ -1,18 +1,22 @@
 from flask import Flask, request, abort
-from google.cloud import storage
+from google.cloud import storage, pubsub_v1
 from google.cloud.sql.connector import connector
 import pymysql
 import logging
 import os
-from dotenv import load_dotenv  # Importing python-dotenv module
+from dotenv import load_dotenv
 
 storage_client = storage.Client()
-load_dotenv()  # Loading environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
 PROJECT_ID = 'ds561-398719'
+TOPIC_NAME = 'error-topic'
 BANNED_COUNTRIES = ["North Korea", "Iran", "Cuba", "Myanmar", "Iraq", "Libya", "Sudan", "Zimbabwe", "Syria"]
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(PROJECT_ID, TOPIC_NAME)
 
 # Database connection function
 def get_db_connection():
@@ -75,7 +79,6 @@ def file_server(path):
     filename = path.lstrip('/')
     filename = filename.replace('bu-ds561-bwong778-hw2-bucket/', '')
     
-    # Extract information from request headers or request data
     country = request.headers.get('X-country')
     client_ip = request.headers.get('X-client-IP')  
     gender = request.headers.get('X-gender') 
@@ -85,10 +88,10 @@ def file_server(path):
     time_of_day = request.headers.get('X-time')  
     requested_file = filename
 
-
-    # Handle request and send response
     if is_banned:
         logging.error('Banned Country:', 400)
+        error_message = f"Access attempt from banned country: {country}"
+        publish_error(error_message)
         insert_error_data(time_of_day, requested_file, 400)
         return 'Banned country', 400
 
@@ -115,6 +118,13 @@ def file_server(path):
         logging.error('Not Implemented:', 501)
         insert_error_data(time_of_day, requested_file, 501)
         return 'Not Implemented', 501
+    
+def publish_error(error_message):
+    data = error_message.encode("utf-8")
+    try:
+        publisher.publish(topic_path, data)
+    except Exception as e:
+        logging.error(f"Failed to publish error: {str(e)}")
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
