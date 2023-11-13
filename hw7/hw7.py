96bbb470eb509beeeffa2659b6a4441cdc4d081a
Brian Wong <brianwong@crc-dot1x-nat-10-239-124-181.bu.edu>
2023-11-13 13:03:49 -0500

    not working

--- hw7/hw7.py
+++ hw7/hw7.py
@@ -1,37 +1,63 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 import re
+import os
 
-# Creds
+# Custom DoFn to extract file name and content
+class ExtractFileNameAndContent(beam.DoFn):
+    def __init__(self, file_pattern):
+        self.file_pattern = file_pattern
+
+    def process(self, element):
+        file_name = os.path.basename(re.search(r'gs://.*/(.*)', self.file_pattern).group(1))
+        yield file_name, element
+
+# Function to extract outgoing links from a file
+def extract_outgoing_links(element):
+    _, content = element
+    return re.findall(r'href="(\d+\.html)"', content)
+
+# Function to extract file and incoming links
+def extract_file_and_links(element):
+    file_name, content = element
+    links = re.findall(r'href="(\d+\.html)"', content)
+    return [(link, file_name) for link in links]
+
+# Function to format the results
+def format_result(element):
+    try:
+        file_name, count = element
+        return f'{file_name}: {count} links'
+    except ValueError:
+        # Handle cases where element does not have two values
+        return f'Error: Element {element} does not match expected format'
+
+#Credentials
 project_id = 'ds561-398719'
 bucket_name = 'bu-ds561-bwong778-hw2-bucket'
 region = 'us-east1'
+file_pattern = f'gs://{bucket_name}/generated_files/*.html'
 
+# Define output file locations
+outgoing_links_output = f'gs://{bucket_name}/results/outgoing_links.txt'
+incoming_links_output = f'gs://{bucket_name}/results/incoming_links.txt'
 
+# Pipeline options
 options = PipelineOptions(
-    runner='DataflowRunner',
+    runner='DataFlowRunner',
     project=project_id,
     region=region,
     temp_location=f'gs://{bucket_name}/temp',
     staging_location=f'gs://{bucket_name}/staging'
 )
 
-def extract_outgoing_links(element):
-    _, content = element
-    return re.findall(r'href="(\d+\.html)"', content)
-
-def extract_file_and_links(element):
-    file_name, content = element
-    links = re.findall(r'href="(\d+\.html)"', content)
-    return [(link, file_name) for link in links]
-
-def format_result(element):
-    file_name, count = element
-    return f'{file_name}: {count} links'
-
 with beam.Pipeline(options=options) as p:
     # Read files and their names
-    files = p | 'ReadFromGCS' >> beam.io.ReadAllFromTextWithFilename(f'gs://{bucket_name}/generated_files/*.html')
+    files = (
+        p
+        | 'ReadFromGCS' >> beam.io.ReadFromText(file_pattern)
+        | 'ExtractFileNameAndContent' >> beam.ParDo(ExtractFileNameAndContent(file_pattern))
+    )
 
     # Outgoing links: Count links in each file
     outgoing_links = (
@@ -40,7 +66,7 @@ with beam.Pipeline(options=options) as p:
         | 'CountOutgoingLinks' >> beam.combiners.Count.PerElement()
         | 'Top5OutgoingLinks' >> beam.combiners.Top.Of(5, key=lambda x: x[1])
         | 'FormatOutgoingResults' >> beam.Map(format_result)
-        | 'PrintTopOutgoingLinks' >> beam.Map(print)
+        | 'WriteOutgoingResults' >> beam.io.WriteToText(outgoing_links_output)
     )
 
     # Incoming links: Count how many times each file is linked to
@@ -50,5 +76,5 @@ with beam.Pipeline(options=options) as p:
         | 'CountIncomingLinks' >> beam.combiners.Count.PerElement()
         | 'Top5IncomingLinks' >> beam.combiners.Top.Of(5, key=lambda x: x[1])
         | 'FormatIncomingResults' >> beam.Map(format_result)
-        | 'PrintTopIncomingLinks' >> beam.Map(print)
+        | 'WriteIncomingResults' >> beam.io.WriteToText(incoming_links_output)
     )
