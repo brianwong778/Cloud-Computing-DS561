707c97105a144bac1acc9172cd1c2a391a5434c1
Brian Wong <brianwong@crc-dot1x-nat-10-239-124-181.bu.edu>
2023-11-17 20:53:04 -0500

    added http-client updates

--- hw8/hw8.py
+++ hw8/hw8.py
@@ -4,79 +4,65 @@ import logging
 import os
 import requests
 
-
 app = Flask(__name__)
-
-logging.basicConfig(filename='app1.log', filemode='w', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 PROJECT_ID = 'ds561-398719'
 TOPIC_NAME = 'error-topic'
 BANNED_COUNTRIES = ["North Korea", "Iran", "Cuba", "Myanmar", "Iraq", "Libya", "Sudan", "Zimbabwe", "Syria"]
 
-#PubSub and Storage
-storage_client = storage.Client()
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(PROJECT_ID, TOPIC_NAME)
 
-
-@app.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'CONNECT', 'OPTIONS', 'TRACE'])
-@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'CONNECT', 'OPTIONS', 'TRACE'])
+@app.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
 def file_server(path):
+    filename = path.lstrip('/')
+    filename = filename.replace('bu-ds561-bwong778-hw2-bucket/', '')
     
+    country = request.headers.get('X-country')
+    if country in BANNED_COUNTRIES:
+        error_message = f"Access attempt from banned country: {country}"
+        publish_error(error_message)
+        logging.error("From banned country")
+        return "Banned country request: 400", 400
     if request.method == 'GET':
-        
-        try:
-        
-            filename = path.lstrip('/')
-            filename = filename.replace('bu-ds561-bwong778-hw2-bucket/', '')
-            
-            country = request.headers.get('X-country')
-            is_banned = country in BANNED_COUNTRIES
-            requested_file = filename
-
-            if is_banned:
-                logging.error("Access attempt from banned country: %s", country)
-                error_message = f"Access attempt from banned country: {country}"
-                publish_error(error_message)
-                return 'Banned country', 400
-
-            bucket = storage_client.bucket('bu-ds561-bwong778-hw2-bucket')         
+            storage_client = storage.Client()
+            bucket = storage_client.bucket('bu-ds561-bwong778-hw2-bucket')       
+           
             blob = bucket.blob(filename) 
-            if not blob.exists():
-                logging.error('File not found:', 404)
-                return 'File not found', 404
-                
-            file_content = blob.download_as_text()
-            zone = get_instance_zone()
-            response = Response(file_content)
-            response.headers['X-VM-Zone'] = zone
-            return response
-        
-        except Exception as e:
-            logging.error("Internal Server Error: %s", str(e))
-            return 'Internal Server Error', 500
+            if(not blob.exists()):
+                logging.error("File does not exist: ", 404)
+                return 'file not found: 404', 404
+            else:
+                file_content = blob.download_as_text()
+                zone = get_instance_zone()
+                response = Response(file_content)
+                response.headers['X-VM-Zone'] = zone
+                return response
+            
     else:
-        logging.error("Not Implemented: %s", requested_file)
-        return 'Not Implemented', 501
-    
+        logging.error(f"Unsupported method: {request.method} for file {filename}")  
+        return "Unsupported method: 501", 501
+
 def get_instance_zone():
     try:
         headers = {"Metadata-Flavor": "Google"}
         r = requests.get("http://metadata.google.internal/computeMetadata/v1/instance/zone", headers=headers)
         if r.status_code == 200:
-            # The response includes the full zone path. We extract just the zone name.
+
             return r.text.split('/')[-1]
         return "Unknown"
     except Exception as e:
         logging.error(f"Failed to get zone: {str(e)}")
         return "Error"
-    
+
 def publish_error(error_message):
     data = error_message.encode("utf-8")
     try:
+        publisher = pubsub_v1.PublisherClient()  
         publisher.publish(topic_path, data)
     except Exception as e:
-        logging.error("Failed to publish error: %s", str(e))
-        
+        logging.error(f"Failed to publish error: {str(e)}")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
